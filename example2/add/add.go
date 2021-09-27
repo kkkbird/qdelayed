c00838012e66b01afcd9dbbfd195b67504d19ff7
Kaiser Li <kaiserli@gmail.com>
2021-09-27 16:02:47 +0800

    fix: compile error in examples

---
+++ example2/add/add.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"strconv"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/kkkbird/qdelayed"
+	log "github.com/sirupsen/logrus"
+)
+
+const (
+	delayDuration = 3 * time.Second
+)
+
+func main() {
+	redisdb := redis.NewClient(&redis.Options{
+		Addr:     "192.168.1.231:31329",
+		Password: "kuyiNo1",
+		DB:       0,
+	})
+	defer redisdb.Close()
+
+	testData := "Hello world"
+
+	delayed := qdelayed.NewRedisDelayed(redisdb, "mydelayed")
+
+	ctx := context.Background()
+
+	for i := 0; ; i++ {
+		for j := 0; j < 10; j++ {
+			delayed.Add(ctx, delayDuration, testData+" "+strconv.Itoa(i)+" "+strconv.Itoa(j))
+		}
+
+		log.Infof("Message delayed, you could see the message after %d second(s)", delayDuration/time.Second)
+		time.Sleep(time.Millisecond * 100)
+	}
+}
---
+++ example2/read/read.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/kkkbird/qdelayed"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	redisdb := redis.NewClient(&redis.Options{
+		Addr:     "192.168.1.231:31329",
+		Password: "kuyiNo1",
+		DB:       0,
+	})
+	defer redisdb.Close()
+
+	delayed := qdelayed.NewRedisDelayed(redisdb, "mydelayed")
+
+	for {
+		rlt, err := delayed.Read(context.Background(), 0, 10)
+
+		if err != nil { // will not return redis.Nil if block 0
+			log.WithError(err).Infof("qdelayed read error")
+			return
+		}
+
+		for i, r := range rlt {
+			log.Infof("%d, %s", i, r.Data)
+		}
+	}
+}
